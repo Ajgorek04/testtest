d3c493385c15eb85b28c6163bb4fe6c5f4554764
Ajgorek04 <igorblendzinski12@gmail.com>
2024-08-23 22:30:12 +0200

    a

--- src/App.jsx
+++ src/App.jsx
@@ -46,7 +46,7 @@ function App() {
     <div className={styles.container}>
       <header className={styles.header}>
         <div>
-          <h1>Do zrobienia</h1>
+          <h1>To do</h1>
           <h2>{getSubHeading(todos.length)}</h2>
         </div>
         {!isFormShown && (
